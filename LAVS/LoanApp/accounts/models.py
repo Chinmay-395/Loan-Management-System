874b83a5c56fa4053f05d3bda492c8b827329ca8
Chinmay-395 <chinmaydali395@gmail.com>
2020-04-05 15:40:24 +0530

    This is shopping cart branch wherein users can visualize all there policies and payments at a collective level

--- LAVS/LoanApp/accounts/models.py
+++ LAVS/LoanApp/accounts/models.py
@@ -1,39 +1,52 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+
 class Applicant(models.Model):
-    user         = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    name         = models.CharField(max_length=200, null=True)
-    phone        = models.CharField(max_length=200, null=True)
-    email        = models.CharField(max_length=200, null=True)
-    CIBIL_score  = models.IntegerField(blank = True, null = True)
-    income       = models.IntegerField(blank=True, null=True)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    CIBIL_score = models.IntegerField(blank=True, null=True)
+    income = models.IntegerField(blank=True, null=True)
     #date_created = models.DateTimeField(auto_now_add=True, null=True)
+
     def __str__(self):
         return self.name
 
+
 class Policy(models.Model):
     BANK = (
-        ('AXIS','AXIS'),
-        ('HDFC','HDFC'),
-        ('ICICI','ICICI'),
+        ('AXIS', 'AXIS'),
+        ('HDFC', 'HDFC'),
+        ('ICICI', 'ICICI'),
     )
     POLICY_TYPES = (
-        ('Home Loan','Home Loan'),
+        ('Home Loan', 'Home Loan'),
         ('Car Loan', 'Car Loan'),
         ('Educational Loan', 'Educational Loan'),
         ('Personal Loan', 'Personal Loan'),
         ('Buisness Loan', 'Buisness Loan'),
         ('Gold Loan', 'Gold Loan'),
     )
-    applicant = models.ForeignKey(Applicant, null=True, on_delete=models.SET_NULL)
-    policy_name = models.CharField(max_length=200, null=True, choices=POLICY_TYPES)
-    bank = models.CharField(max_length=200, null=True, blank=True, choices=BANK)
+    applicant = models.ForeignKey(
+        Applicant, null=True, on_delete=models.SET_NULL)
+    policy_name = models.CharField(
+        max_length=200, null=True, choices=POLICY_TYPES)
+    bank = models.CharField(max_length=200, null=True,
+                            blank=True, choices=BANK)
     Tenure = models.IntegerField(blank=True, null=True)
     Processing_Fees = models.IntegerField(blank=True, null=True)
     loan_amount = models.IntegerField(blank=True, null=True)
     interest_rate = models.IntegerField(default=5, blank=True, null=True)
 
-
     def __str__(self):
         return self.policy_name
+
+
+class OrderPolicy(models.Model):
+    """
+    This is similar to JustDjango the shopping-cart tutorial
+    """
+    pass
